b2fc4539f84f16c37158ba929fc7e5bb905d93c1
Ayush Yadav <aayooosh2211@gmail.com>
2024-10-09 14:56:40 +0530

    Update case study (swiggy) (2).sql

--- case study (swiggy) (2).sql
+++ case study (swiggy) (2).sql
@@ -76,26 +76,3 @@ from (
   select month, revenue,lag(revenue,1) over(order by month ) as prev from sales
 ) t ;
 
--- Q8.find the favourite food of every customer
-
-with temp as 
-(  
-   select o.user_id,od.f_id, count(*) as frequency
-   from orders o
-   join order_details od
-   on o.order_id=od.order_id
-   group by o.user_id,od.f_id
- )
-select u.name,f.f_name, t1.frequency from
-temp t1
-join users u
-on u.user_id=t1.user_id
-join food f
-on f.f_id=t1.f_id
-where t1.frequency = (
-      select max(frequency)
-      from temp t2
-      where t2.user_id=t1.user_id
-      )
-
-group by u.name,f.f_name, t1.frequency
