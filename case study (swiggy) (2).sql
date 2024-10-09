c6b2df047bbe5cb171f7313572abd275bcc6ed5b
Ayush Yadav <aayooosh2211@gmail.com>
2024-10-09 14:58:01 +0530

    case study (swiggy).sql

--- case study (swiggy) (2).sql
+++ case study (swiggy) (2).sql
@@ -1,23 +1,20 @@
 
 -- Q1. find the name of the customers who never ordered
+
 select name 
 from users
 where user_id not in (select user_id from orders);
 
-
-
-
-
 -- Q2. find average amount of dishes
+
 select f.f_name,avg(price) as avg_price
 from menu m
 join food f
 on m.f_id=f.f_id
 group by f.f_name;
 
-
-
 -- Q3. find top restaurant in each month based on no of orders
+
 select r.r_name, monthname(date) as month,count(*) as total_order
 from orders o
 join restaurants r
@@ -25,7 +22,9 @@ on o.r_id = r.r_id
 where monthname(date) like 'june'
 group by r.r_name,monthname(date)
 order by count(*) desc limit 1;
+
 -- Q4.Restaurants with monthly sales >x
+
 select r.r_name ,monthname(date) as month ,sum(amount) as revenue
 from orders o
 join restaurants r
@@ -37,6 +36,7 @@ order by revenue desc;
 
 
 -- Q5. ordet details of specific customer in given date range
+
  select o.order_id,r.r_name,f.f_name
  from orders o
  join restaurants r
@@ -52,6 +52,7 @@ order by revenue desc;
 date between '2022-06-10'and '2022-07-10';
 
 -- Q 6. find the restaurant with most repeated customers
+
 select r.r_name,count(*) as loyal_customer
 from (
 select r_id, user_id, count(*) as visits
@@ -62,17 +63,3 @@ join restaurants r
 on r.r_id=t.r_id
 group by r.r_name
 order by loyal_customer desc limit 1;
--- Q7.  find month over month growth of swiggy......
-
- select month ,((revenue-prev)/prev)*100 as growth
-from (
- with sales as
- (
-   select month(date) as 'month', sum(amount) as 'revenue'
-   from orders
-   group by month
-   order by month
-  ) 
-  select month, revenue,lag(revenue,1) over(order by month ) as prev from sales
-) t ;
-
